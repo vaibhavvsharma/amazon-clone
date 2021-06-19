8312780cc449af935f0f747863dd7ff33cc70c28
sublime <vaibhavvsharma@hotmail.com>
2021-06-19 22:14:34 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCh_WSxk6VeFn7Q5qJ0K0nNTxRRZiW3OV4",
+  authDomain: "clone-20911.firebaseapp.com",
+  projectId: "clone-20911",
+  storageBucket: "clone-20911.appspot.com",
+  messagingSenderId: "910115690480",
+  appId: "1:910115690480:web:dcbc030e1dcd02ee8f2f35",
+  measurementId: "G-W7XKKWX228"
+};
